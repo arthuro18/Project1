5e324a442a720e4c11921981b5c00adb071500e3
mario <mario_gongora_leon@hotmail.com>
2015-11-17 19:13:54 -0600

    se agrega nueva configuracion y funcionalidad

--- SpringMVC/src/main/java/com/spring/springapp/web/HelloController.java
+++ SpringMVC/src/main/java/com/spring/springapp/web/HelloController.java
@@ -1,6 +1,7 @@
 package com.spring.springapp.web;
 
 import java.io.IOException;
+import java.util.Date;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -15,14 +16,16 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class HelloController {
 
-    protected final Log logger = LogFactory.getLog(getClass());
+	protected final Log logger = LogFactory.getLog(getClass());
 
-    @RequestMapping(value="/hello.htm")
-    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+	@RequestMapping(value = "/hello.htm")
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
-        logger.info("Returning hello view");
+		String now = (new Date()).toString();
+		logger.info("Returning hello view with " + now);
 
-        return new ModelAndView("hello.jsp");
-    }
+		return new ModelAndView("hello", "now", now);
+
+	}
 }
